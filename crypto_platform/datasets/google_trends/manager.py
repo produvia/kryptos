ef8c8d91f731c2fa4c399147b545d346ba8db538
Cam Sweeney <cpsweene@gmail.com>
2018-03-11 13:22:50 -0700

    use config start/end for trend timeframe

--- crypto_platform/datasets/google_trends/manager.py
+++ crypto_platform/datasets/google_trends/manager.py
@@ -3,6 +3,8 @@ import pandas as pd
 from pytrends.request import TrendReq
 from logbook import Logger
 
+from crypto_platform.config import CONFIG
+
 log = Logger('GoogleTrendDataManager')
 
 pytrends = TrendReq(hl='en-US', tz=360)
@@ -15,7 +17,9 @@ class GoogleTrendDataManager(object):
         self.kwds = keywords
         self.trends = TrendReq(hl='en-US', tz=360)
 
-        self.trends.build_payload(keywords, cat=0, timeframe='today 5-y', geo='', gprop='')
+        timeframe = str(CONFIG.START.date()) + ' ' + str(CONFIG.END.date())
+
+        self.trends.build_payload(keywords, cat=0, timeframe=timeframe, geo='', gprop='')
 
         df = self.trends.interest_over_time()
         df.index = pd.to_datetime(df.index, unit='s')
