1837511210a787531d394fcd66c46c3ca7805689
Cam Sweeney <cpsweene@gmail.com>
2018-03-11 19:13:54 -0700

    begin refactor of algo factory utils.algo.py: New module to manage algo creation for catalyst trends.py: utilize algo module for boilerplate code google_trends/manager.py: record within data manager class

--- crypto_platform/scripts/trends.py
+++ crypto_platform/scripts/trends.py
@@ -2,8 +2,7 @@ from catalyst import run_algorithm
 from catalyst.api import record, set_benchmark, symbol, get_open_orders, order, cancel_order, get_dataset
 from catalyst.exchange.exchange_errors import PricingDataNotLoadedError
 
-from crypto_platform.utils import load, viz
-from crypto_platform.analysis.indicators import TAAnalysis
+from crypto_platform.utils import load, viz, algo
 from crypto_platform.config import CONFIG
 from crypto_platform.datasets.google_trends.manager import GoogleTrendDataManager
 from logbook import Logger
@@ -32,32 +31,10 @@ def run(keywords, asset):
     click.secho('Analysis Google Trends:\n{}'.format(keywords), fg='white')
 
     def initialize(context):
-
-        context.ORDER_SIZE = 10
-        context.SLIPPAGE_ALLOWED = 0.05
-        context.BARS = 365
-
-        context.swallow_errors = True
-        context.errors = []
-
-        context.ASSET_NAME = CONFIG.ASSET
-        context.asset = symbol(context.ASSET_NAME)
-        context.market = symbol(CONFIG.ASSET)
-
-        set_benchmark(context.asset)
+        algo.initialze_from_config(context)
 
     def handle_data(context, data):
-        date = context.blotter.current_dt.date()
-
-        price = data.current(context.asset, 'price')
-        record_payload = {'price': price}
-
-        if date in trends.df.index:
-            for k in keywords:
-                current_val = trends.kw_by_date(k, date)
-                record_payload[k] = current_val
-
-        record(**record_payload)
+        algo.record_data(context, data, trends)
 
     def analyze(context, results):
         viz.plot_metric(results, 'price', pos=211, label='Price')
@@ -65,21 +42,7 @@ def run(keywords, asset):
             viz.plot_metric(results, k, pos=212, y_label='Google Trends', label=k)
         plt.legend()
 
-    try:
-        run_algorithm(
-            capital_base=CONFIG.CAPITAL_BASE,
-            data_frequency=CONFIG.DATA_FREQUENCY,
-            initialize=initialize,
-            handle_data=handle_data,
-            analyze=analyze,
-            exchange_name=CONFIG.BUY_EXCHANGE,
-            base_currency=CONFIG.BASE_CURRENCY,
-            start=CONFIG.START,
-            end=CONFIG.END,
-        )
-    except PricingDataNotLoadedError:
-        log.info('Ingesting required exchange bundle data')
-        load.ingest_exchange(CONFIG)
+    algo.run_algo(initialize, handle_data, analyze)
 
     viz.add_legend()
     viz.show_plot()
---
+++ crypto_platform/utils/algo.py
@@ -0,0 +1,67 @@
+from catalyst import run_algorithm
+from catalyst.api import symbol, set_benchmark, record
+from catalyst.exchange.exchange_errors import PricingDataNotLoadedError
+from logbook import Logger
+
+from crypto_platform.config import CONFIG
+from crypto_platform.utils import load
+
+log = Logger('AlgoFactory')
+
+
+def initialze_from_config(context):
+    context.asset = symbol(CONFIG.ASSET)
+    context.market = symbol(CONFIG.ASSET)
+    set_benchmark(context.asset)
+
+    context.ORDER_SIZE = 10
+    context.SLIPPAGE_ALLOWED = 0.05
+    context.BARS = 365
+
+    context.errors = []
+
+    for k, v in CONFIG.__dict__.items():
+        if '__' not in k:
+            setattr(context, k, v)
+
+
+def record_data(context, data, data_manager=None):
+    price = data.current(context.asset, 'price')
+    cash = context.portfolio.cash
+
+    data_payload = {}
+    if data_manager is not None:
+        data_payload = data_manager.record_data(context, data)
+
+    # Save values for later inspection
+    record(price=price, cash=cash, **data_payload)
+
+    # Get price, open, high, low, close
+    prices = data.history(
+        context.asset,
+        bar_count=context.BARS,
+        fields=['price', 'open', 'high', 'low', 'close'],
+        frequency='1d')
+
+    # Save the prices and analysis to send to analyze
+    context.prices = prices
+    context.price = data.current(context.asset, 'price')
+
+
+
+def run_algo(initialize, handle_data, analyze):
+    try:
+        run_algorithm(
+            capital_base=CONFIG.CAPITAL_BASE,
+            data_frequency=CONFIG.DATA_FREQUENCY,
+            initialize=initialize,
+            handle_data=handle_data,
+            analyze=analyze,
+            exchange_name=CONFIG.BUY_EXCHANGE,
+            base_currency=CONFIG.BASE_CURRENCY,
+            start=CONFIG.START,
+            end=CONFIG.END,
+        )
+    except PricingDataNotLoadedError:
+        log.info('Ingesting required exchange bundle data')
+        load.ingest_exchange(CONFIG)
