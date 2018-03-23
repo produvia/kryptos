87e7a6b13e388b9c93e047105d0a90b87bec3d89
Cam Sweeney <cpsweene@gmail.com>
2018-03-23 02:20:33 -0700

    restore the implemented indicators and their trade signals

--- crypto_platform/strategy/indicators/technical.py
+++ crypto_platform/strategy/indicators/technical.py
@@ -13,6 +13,7 @@ from crypto_platform.strategy.indicators import AbstractIndicator
 
 log = Logger('INDICATOR')
 
+
 def get_indicator(name):
     subclass = globals().get(name)
     if subclass is not None:
@@ -22,22 +23,23 @@ def get_indicator(name):
 
 
 class TAIndicator(AbstractIndicator):
-    def __init__(self, name):
+    def __init__(self, name, **kw):
         """Factory for creating an indicator using the ta-lib library
-        
+
         The costructor is passed the name of the indicator.
         The calculation is performed at each iteration and is recored
         and plotted based on the ta-lib function's outputs.
 
         To signal trade opportunities, subclassed objects can implement
         the signals_buy and signals_sell methods.
-        
+
         Extends:
             Indicator): def __init__(self, name
         """
         self.name = name.upper()
         self.data = None
         self.outputs = None
+        self.kw = kw
 
     @property
     def func(self):
@@ -58,7 +60,7 @@ class TAIndicator(AbstractIndicator):
             **kw {[type]} -- [description]
         """
         self.data = df
-        self.outputs = self.func(df, **kw)
+        self.outputs = self.func(df, **self.kw)
 
         if isinstance(self.outputs, pd.Series):
             self.outputs = self.outputs.to_frame(self.name)
@@ -79,8 +81,9 @@ class TAIndicator(AbstractIndicator):
             ignore = []
         for col in [c for c in list(self.outputs) if c not in ignore]:
             log.error(col)
-            viz.plot_column(results, col, pos, y_label=y_label, label=col)
+            ax = viz.plot_column(results, col, pos, y_label=y_label, label=col)
         plt.legend()
+        return ax
 
     @property
     def signals_buy(self):
@@ -93,54 +96,48 @@ class TAIndicator(AbstractIndicator):
         pass
 
 
-# class BBANDS(TAIndicator):
-#     def __init__(self):
-#         super(BBANDS, self).__init__('BBANDS')
+class BBANDS(TAIndicator):
+    def __init__(self, matype=ta.MA_Type.T3):
+        super(BBANDS, self).__init__('BBANDS', matype=matype)
 
-#     def plot(self, results, pos):
-#         viz.plot_column(results, 'price', pos, label='price')
-#         super().plot(results, pos)
+    def plot(self, results, pos):
+        super().plot(results, pos)
 
-#     @property
-#     def current_price(self):
-#         return self.data.close[-1]
+    @property
+    def current_price(self):
+        return self.data.close[-1]
 
-#     @property
-#     def signals_buy(self):
-#         # log.error('**************************************')
-#         # log.error('{} -- {}'.format(self.current_price, self.outputs.upperband[-1]))
-#         if self.current_price > self.outputs.upperband[-1]:
-#             log.error('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-#             return True
-#         return False
+    @property
+    def signals_buy(self):
+        if self.current_price > self.outputs.upperband[-1]:
+            return True
+        return False
 
-#     @property
-#     def signals_sell(self):
-#         # log.error('**************************************')
-
-#         if self.current_price < self.outputs.lowerband[-1]:
-#             return True
-#         return False
+    @property
+    def signals_sell(self):
+        if self.current_price < self.outputs.lowerband[-1]:
+            return True
+        return False
 
 
-# class PSAR(TAIndicator):
-#     def __init__(self):
-#         super(PSAR, self).__init__('SAR')
+class SAR(TAIndicator):
+    def __init__(self):
+        super(SAR, self).__init__('SAR')
 
-#     @property
-#     def current_price(self):
-#         return self.data.close[-1]
+    @property
+    def current_price(self):
+        return self.data.close[-1]
 
-#     @property
-#     def signals_buy(self):
-#         return self.current_price > self.psar[-1]
+    @property
+    def signals_buy(self):
+        return self.current_price > self.outputs.SAR[-1]
 
-#     @property
-#     def signals_sell(self):
-#         if self.current_price < self.psar[-1]:
-#             log.info('Closing position due to PSAR')
-#             return True
-#         return False
+    @property
+    def signals_sell(self):
+        if self.current_price < self.outputs.SAR[-1]:
+            log.info('Closing position due to PSAR')
+            return True
+        return False
 
 
 class MACD(TAIndicator):
@@ -161,233 +158,138 @@ class MACD(TAIndicator):
         return self.outputs.macd[-1] < self.outputs.macdsignal[-1]
 
 
-# class MACDFIX(MACD):
+class MACDFIX(TAIndicator):
 
-#     def __init__(self):
-#         super(MACD, self).__init__('MACDFIX')
+    def __init__(self):
+        super(MACDFIX, self).__init__('MACDFIX')
 
+    def plot(self, results, pos):
+        super().plot(results, pos, ignore=['macdhist'])
+        viz.plot_bar(results, 'macdhist', pos)
 
-#     # def calculate(self, prices):
-#     #     self.macd, self.macd_signal, self.macd_hist = ta.MACDFIX(
-#     #         prices.close.as_matrix(), signalperiod=CONFIG.MACD_SIGNAL)
+    @property
+    def signals_buy(self):
+        return self.outputs.macd[-1] > self.outputs.macdsignal[-1]
 
-#     #     self.macd_test = np.where((self.macd > self.macd_signal), 1, 0)
-
-
-# class OBV(TAIndicator):
-#     def __init__(self):
-#         super(OBV, self).__init__('OBV')
-
-#     @property
-#     def signals_buy(self):
-#         log.error('checking obv bs')
-#         log.error('{} > {} ??'.format(self.obv[-1], self.obv[-2]))
-#         return self.outputs.obv[-1] > self.outputs.obv[-2]
-
-#     @property
-#     def signals_sell(self):
-#         return self.outputs.obv[-1] < self.outputs.obv[-2]
+    @property
+    def signals_sell(self):
+        return self.outputs.macd[-1] < self.outputs.macdsignal[-1]
 
 
-# class RSI(TAIndicator):
-#     def __init__(self):
-#         super(RSI, self).__init__('RSI')
+class OBV(TAIndicator):
+    def __init__(self):
+        super(OBV, self).__init__('OBV')
 
-#     def calculate(self, prices):
-#         self.rsi = ta.RSI(prices.close.as_matrix(), CONFIG.RSI_PERIOD)
+    @property
+    def signals_buy(self):
+        log.error('checking obv bs')
+        log.error('{} > {} ??'.format(self.outputs.OBV[-1], self.outputs.OBV[-2]))
+        return self.outputs.OBV[-1] > self.outputs.OBV[-2]
 
-#     def record(self):
-#         record(rsi=self.rsi[-1], overbought=self.overbought, oversold=self.oversold)
-
-#     def plot(self, results, pos):
-#         y_label = 'RSI'
-#         ax = viz.plot_column(results, 'rsi', pos, y_label=y_label, label='rsi')
-
-#         overbought_line = [CONFIG.RSI_OVER_BOUGHT for i in results.index]
-#         oversold_line = [CONFIG.RSI_OVER_SOLD for i in results.index]
-#         ax.plot(results.index, overbought_line)
-#         ax.plot(results.index, oversold_line)
-
-#         overboughts = results[results['overbought']]
-#         oversolds = results[results['oversold']]
-#         viz.plot_points(overboughts, pos, y_val='rsi', color='red', label='overbought')
-#         viz.plot_points(oversolds, pos, y_val='rsi', label='oversold')
-
-#         plt.legend()
-
-#     @property
-#     def overbought(self):
-#         # RSI OVER BOUGHT & Decreasing
-#         return self.rsi[-2] >= CONFIG.RSI_OVER_BOUGHT and self.rsi[-1] < CONFIG.RSI_OVER_BOUGHT
-
-#     @property
-#     def oversold(self):
-#         # RSI OVER SOLD & Increasing
-#         return self.rsi[-2] <= CONFIG.RSI_OVER_SOLD and self.rsi[-1] > CONFIG.RSI_OVER_SOLD
-
-#     @property
-#     def signals_buy(self):
-#         # crosses to above oversold
-#         return self.rsi[-2] <= CONFIG.RSI_OVER_SOLD and self.rsi[-1] > CONFIG.RSI_OVER_SOLD
-
-#     @property
-#     def signals_sell(self):
-#         # crosses to below overbought
-#         return self.rsi[-2] >= CONFIG.RSI_OVER_BOUGHT and self.rsi[-1] < CONFIG.RSI_OVER_BOUGHT
-
-#     @property
-#     def sma_rsi(self):
-#         return ta.SMA(self.rsi.as_matrix(), CONFIG.RSI_AVG_PERIOD)
-
-#     @property
-#     def sma_fast(self):
-#         self.results['sma_fast'] = ta.SMA(self.prices.close.as_matrix(), CONFIG.SMA_FAST)
-#         return self.results['sma_fast']
-
-#     @property
-#     def sma_slow(self):
-#         return ta.SMA(self.pricesclose.as_matrix(), CONFIG.SMA_SLOW)
-
-#     @property
-#     def sma_test(self):
-#         return np.where(self.sma_fast > self.sma_slow, 1, 0)
+    @property
+    def signals_sell(self):
+        return self.outputs.OBV[-1] < self.outputs.OBV[-2]
 
 
-# class STOCH(TAIndicator):
-#     """docstring for STOCH"""
+class RSI(TAIndicator):
+    def __init__(self, timeperiod=14):
+        super(RSI, self).__init__('RSI', timeperiod=timeperiod)
 
-#     def __init__(self):
-#         super(STOCH, self).__init__()
+    def record(self):
+        super().record()
+        record(overbought=self.overbought, oversold=self.oversold)
 
-#     def calculate(self, prices):
-#         self.stoch_k, self.stoch_d = ta.STOCH(
-#             prices.high.as_matrix(), prices.low.as_matrix(),
-#             prices.close.as_matrix(), slowk_period=CONFIG.STOCH_K_PERIOD,
-#             slowd_period=CONFIG.STOCH_D_PERIOD)
+    def plot(self, results, pos):
+        y_label = 'RSI'
+        ax = viz.plot_column(results, 'RSI', pos, y_label=y_label, label='RSI')
 
-#     def record(self):
-#         record(
-#             stoch_k=self.stoch_k[-1],
-#             stoch_d=self.stoch_d[-1],
-#             stoch_overbought=self.overbought,
-#             stoch_oversold=self.oversold)
+        overbought_line = [CONFIG.RSI_OVER_BOUGHT for i in results.index]
+        oversold_line = [CONFIG.RSI_OVER_SOLD for i in results.index]
+        ax.plot(results.index, overbought_line)
+        ax.plot(results.index, oversold_line)
 
-#     def plot(self, results, pos):
-#         y_label = 'STOCH'
-#         viz.plot_column(results, 'stoch_k', pos, y_label=y_label, label='stoch_k')
-#         ax = viz.plot_column(results, 'stoch_d', pos, y_label=y_label, label='stoch_d')
+        overboughts = results[results['overbought']]
+        oversolds = results[results['oversold']]
+        viz.plot_points(overboughts, pos, y_val='RSI', color='red', label='overbought')
+        viz.plot_points(oversolds, pos, y_val='RSI', label='oversold')
 
-#         overbought_line = [CONFIG.STOCH_OVER_BOUGHT for i in results.index]
-#         oversold_line = [CONFIG.STOCH_OVER_SOLD for i in results.index]
-#         ax.plot(results.index, overbought_line)
-#         ax.plot(results.index, oversold_line)
+        plt.legend()
 
-#         overboughts = results[results['stoch_overbought']]
-#         oversolds = results[results['stoch_oversold']]
-#         viz.plot_points(overboughts, pos, y_val='stoch_k', color='red', label='overbought')
-#         viz.plot_points(oversolds, pos, y_val='stoch_k', label='oversold')
+    @property
+    def overbought(self):
+        # RSI OVER BOUGHT & Decreasing
+        return self.outputs.RSI[-2] >= CONFIG.RSI_OVER_BOUGHT and self.outputs.RSI[-1] < CONFIG.RSI_OVER_BOUGHT
 
-#         plt.legend()
+    @property
+    def oversold(self):
+        # RSI OVER SOLD & Increasing
+        return self.outputs.RSI[-2] <= CONFIG.RSI_OVER_SOLD and self.outputs.RSI[-1] > CONFIG.RSI_OVER_SOLD
 
-#     @property
-#     def overbought(self):
-#         return self.stoch_d[-2] > CONFIG.STOCH_OVER_BOUGHT and self.stoch_d[-1] < self.stoch_d[-2]
+    @property
+    def signals_buy(self):
+        # crosses to above oversold
+        return self.oversold
 
-#     @property
-#     def oversold(self):
-
-#         return self.stoch_d[-2] < CONFIG.STOCH_OVER_SOLD and self.stoch_d[-1] > self.stoch_d[-2]
-
-#     @property
-#     def signals_buy(self):
-#         return self.oversold
-
-#     @property
-#     def signals_sell(self):
-#         return self.overbought
+    @property
+    def signals_sell(self):
+        # crosses to below overbought
+        return self.overbought
 
 
-# class SMA(object):
-#     def __init__(self, prices):
-#         super(SMA, self).__init__()
-#         self.prices = prices
-        # self.calculate()
+class STOCH(TAIndicator):
+    """docstring for STOCH"""
 
-#     def calculate(self):
-#         self.slow = ta.SMA(self.prices.close.as_matrix())
-#         self.fast = ta.SMA(self.prices.close.as_matrix(), CONFIG.SMA_FAST)
+    def __init__(self, rsi_period):
+        super(STOCH, self).__init__('STOCH')
+
+    def record(self):
+        super().record()
+        record(stoch_overbought=self.overbought, stoch_oversold=self.oversold)
+
+    def plot(self, results, pos):
+        ax = super().plot(results, pos)
+
+        overbought_line = [CONFIG.STOCH_OVER_BOUGHT for i in results.index]
+        oversold_line = [CONFIG.STOCH_OVER_SOLD for i in results.index]
+
+        ax.plot(results.index, overbought_line)
+        ax.plot(results.index, oversold_line)
+
+        overboughts = results[results['stoch_overbought']]
+        oversolds = results[results['stoch_oversold']]
+        viz.plot_points(overboughts, pos, y_val='slowd', color='red', label='overbought')
+        viz.plot_points(oversolds, pos, y_val='slowk', label='oversold')
+
+        plt.legend()
+
+    @property
+    def overbought(self):
+        return self.outputs.slowd[-2] > CONFIG.STOCH_OVER_BOUGHT and self.outputs.slowd[-1] < self.outputs.slowd[-2]
+
+    @property
+    def oversold(self):
+        return self.outputs.slowd[-2] < CONFIG.STOCH_OVER_SOLD and self.outputs.slowd[-1] > self.outputs.slowd[-2]
+
+    @property
+    def signals_buy(self):
+        return self.oversold
+
+    @property
+    def signals_sell(self):
+        return self.overbought
+
+
+# class SMA(TAIndicator):
+#     def __init__(self, timeperiod=30):
+#         super(SMA, self).__init__('SMA', timeperiod=timeperiod)
 
 #     def plot(self, results, pos):
-#         y_label = 'SMA'
-#         ax = viz.plot_column(results, 'sma_slow', pos, y_label=y_label, label='sma_slow')
-#         viz.plot_column(results, 'sma_fast', pos, y_label=y_label, label='sma_fast')
-#         viz.plot_column(results, 'price', pos, y_label=y_label, label='sma_fast')
+#         viz.plot_column(results, 'price', pos)
+#         super().plot(results, pos)
 
-#         viz.plot_buy_sells(results, pos, y_val='price' )
+    # def signals_buy(self):
+    #     return self.fast > self.slow
 
-#         plt.legend()
+    # def signals_sell(self):
+    #     return self.slow
 
-#     def record(self):
-#         record(sma_slow=self.slow[-1], sma_fast=self.fast[-1])
-
-#     def signals_buy(self):
-#         return self.fast > self.slow
-
-#     def signals_sell(self):
-#         return self.slow
-
-
-#     # Stochastics OVER BOUGHT & Decreasing
-#     df['stoch_over_bought'] = np.where(
-#         (df.stoch_k > CONFIG.STOCH_OVER_BOUGHT) & (
-#             df.stoch_k > df.stoch_k.shift(1)), 1, 0)
-
-#     # Stochastics OVER SOLD & Increasing
-#     df['stoch_over_sold'] = np.where(
-#         (df.stoch_k < CONFIG.STOCH_OVER_SOLD) & (
-#             df.stoch_k > df.stoch_k.shift(1)), 1, 0)
-
-#     # Stochastics %K %D
-#     # %K = (Current Close - Lowest Low)/(Highest High - Lowest Low) * 100
-#     # %D = 3-day SMA of %K
-#     df['stoch_k'], df['stoch_d'] = ta.STOCH(
-#         prices.high.as_matrix(), prices.low.as_matrix(),
-#         prices.close.as_matrix(), slowk_period=CONFIG.STOCH_K,
-#         slowd_period=CONFIG.STOCH_D)
-
-#     # Stochastics OVER BOUGHT & Decreasing
-#     df['stoch_over_bought'] = np.where(
-#         (df.stoch_k > CONFIG.STOCH_OVER_BOUGHT) & (
-#             df.stoch_k > df.stoch_k.shift(1)), 1, 0)
-
-#     # Stochastics OVER SOLD & Increasing
-#     df['stoch_over_sold'] = np.where(
-#         (df.stoch_k < CONFIG.STOCH_OVER_SOLD) & (
-#             df.stoch_k > df.stoch_k.shift(1)), 1, 0)
-
-
-# def stoch_rsi(prices, df):
-#     df['fast_k'], df['fast_d'] = ta.STOCHRSI(
-#         prices.close.as_matrix(),
-#         timeperiod=CONFIG.TIMEPERIOD,
-#         fastk_period=CONFIG.FASTK_PERIOD,
-#         fastd_period=CONFIG.FASTD_PERIOD,
-#         fastd_matype=CONFIG.FASTD_MATYPE)
-
-#     # Stochastics %K %D
-#     # %K = (Current Close - Lowest Low)/(Highest High - Lowest Low) * 100
-#     # %D = 3-day SMA of %K
-#     # df['stoch_k'], df['stoch_d'] = ta.STOCHRSI(
-#     #     prices.high.as_matrix(), prices.low.as_matrix(),
-#     #     prices.close.as_matrix(), slowk_period=CONFIG.STOCH_K,
-#     #     slowd_period=CONFIG.STOCH_D)
-
-#     # Stochastics OVER BOUGHT & Decreasing
-#     df['stoch_over_bought'] = np.where(
-#         (df.fast_k > CONFIG.STOCH_OVER_BOUGHT) & (
-#             df.fast_k > df.fast_k.shift(1)), 1, 0)
-
-#     # Stochastics OVER SOLD & Increasing
-#     df['stoch_over_sold'] = np.where(
-#         (df.fast_k < CONFIG.STOCH_OVER_SOLD) & (
-#             df.fast_k > df.fast_k.shift(1)), 1, 0)
