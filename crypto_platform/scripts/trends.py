c0c431867cbd36897c4391c82052ee96863de202
Cam Sweeney <cpsweene@gmail.com>
2018-04-04 03:07:07 -0700

    use Strategy object for trends and bchain_activity scripts

--- crypto_platform/scripts/trends.py
+++ crypto_platform/scripts/trends.py
@@ -1,11 +1,8 @@
 import click
-import matplotlib.pyplot as plt
 from logbook import Logger
 
-
-from crypto_platform.utils import viz, algo
 from crypto_platform.config import CONFIG
-from crypto_platform.datasets.google_trends.manager import GoogleTrendDataManager
+from crypto_platform.strategy import Strategy
 
 
 log = Logger('Blockchain Activity')
@@ -24,25 +21,13 @@ def run(keywords, asset):
     keywords = list(keywords)
     if asset:
         keywords.append(CONFIG.ASSET.replace('_', ' '))
-    trends = GoogleTrendDataManager(keywords)
+
+    strat = Strategy()
+    strat.use_dataset('google', columns=keywords)
+
     click.secho('Analysis Google Trends:\n{}'.format(keywords), fg='white')
 
-    def initialize(context):
-        algo.initialze_from_config(context)
-
-    def handle_data(context, data):
-        algo.record_data(context, data, trends)
-
-    def analyze(context, results):
-        viz.plot_column(results, 'price', pos=211, label='Price')
-        for k in keywords:
-            viz.plot_column(results, k, pos=212, y_label='Google Trends', label=k)
-        plt.legend()
-
-    algo.run_algo(initialize, handle_data, analyze)
-
-    viz.add_legend()
-    viz.show_plot()
+    strat.run()
 
 
 if __name__ == '__main__':
