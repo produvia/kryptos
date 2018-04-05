9bfcb99a4d59b27fc80c118cb243c7be03a8a9d5
Cam Sweeney <cpsweene@gmail.com>
2018-04-05 13:55:52 -0700

    add stoch params and comments setting config via api #3

--- examples/api_example.py
+++ examples/api_example.py
@@ -22,6 +22,14 @@ strat.use_dataset('quandl', columns=['MKTCP'])
 strat.use_dataset('google', columns=['bitcoin futures'])
 strat.add_data_indicator('google', 'relchange', col='bitcoin futures')
 
+# trading config can be set via json or api
+# Note that minute data is not supported for external datasets
+# strat.trading_info['CAPITAL_BASE'] = 10000
+# strat.trading_info['DATA_FREQ'] = 'minute'
+# strat.trading_info['HISTORY_FREQ'] = '1m'
+# strat.trading_info['START'] = '2017-12-10'
+# strat.trading_info['END'] = '2017-12-11'
+
 
 @strat.init
 def init(context):
@@ -35,8 +43,8 @@ def handle_data(context, data):
     context.i += 1
 
 
-@strat.analyze
-def analyze(context, results):
+@strat.analyze(num_plots=0)
+def analyze(context, results, pos):
     ending_cash = results.cash[-1]
     log.info('Ending cash: ${}'.format(ending_cash))
     log.info('Completed for {} trading periods'.format(context.i))
