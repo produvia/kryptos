87b3bc8e3d8d360fdb9169fad1e7322b6f4d0a5b
Cam Sweeney <cpsweene@gmail.com>
2018-04-30 15:16:39 -0700

    initial work for web app. Add rpc support for running strat #56

---
+++ web/app.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""The flask app module, containing the app factory function."""
+from flask import Flask
+
+from web import web, rpc
+from web.extensions import jsonrpc
+from web.settings import ProdConfig
+
+
+def create_app(config_object=ProdConfig):
+    """An application factory, as explained here: http://flask.pocoo.org/docs/patterns/appfactories/.
+
+    :param config_object: The configuration object to use.
+    """
+    app = Flask(__name__.split('.')[0])
+    app.config.from_object(config_object)
+    register_extensions(app)
+    register_blueprints(app)
+    return app
+
+
+def register_extensions(app):
+    """Register Flask extensions.
+
+    Flask-Assistant does not need to be initalized here if declared as a blueprint.
+    Other extensions such as flask-sqlalchemy and flask-migrate are reigstered here.
+    If the entire flask app consists of only the Assistant, uncomment the code below.
+    """
+    jsonrpc.init_app(app)
+    jsonrpc._register_browse(app)
+    return None
+
+
+def register_blueprints(app):
+    """Register Flask blueprints.
+
+    When Flask-Assistant is used to create a blueprint within a standard flask app,
+    it must be registered as such, rather that with init_app().
+
+    If the entire flask app consists of only the Assistant, comment out the code below.
+    """
+    app.register_blueprint(web.views.blueprint)
+    app.register_blueprint(rpc.views.api)
+    return None
---
+++ web/web/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""The web module: a standard flask blueprint creating a landing page for the assistant"""
+from . import views
