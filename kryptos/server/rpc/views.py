0f689a933fd82c0fb8451118b05d144d9cad1af8
Cam Sweeney <cpsweene@gmail.com>
2018-05-01 21:49:43 -0700

    add rpc method to get strategy job status

--- kryptos/server/rpc/views.py
+++ kryptos/server/rpc/views.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 import json
-from flask import Blueprint, Response
-from rq import Queue, Connection
+from flask import Blueprint
+from rq import Queue
 
 from kryptos.platform.strategy import Strategy
 from kryptos.server.extensions import jsonrpc
 from kryptos.server.utils.worker import conn
 
-
-api = Blueprint("api", __name__)
+# api blueprint currently not actually used
+# jsonrpc acts as its own blueprint, asigning all method to api/
+api = Blueprint("api", __name__, url_prefix="/api")
 
 
 def queue_strat(strat_json):
@@ -23,4 +24,12 @@ def run(strat_json):
     q = Queue(connection=conn)
     result = q.enqueue(queue_strat, strat_json)
     resp = {"status": "success", "data": {"strat_id": result.get_id()}}
+    return resp
+
+
+@jsonrpc.method("Strat.status")
+def get_status(strat_id):
+    q = Queue(connection=conn)
+    job = q.fetch_job(strat_id)
+    resp = {"status": job.status, "data": {"strat_id": job.get_id()}}
     return resp
