1802d54ec640c26ae133ad9829c817c221b8ac59
Cam Sweeney <cpsweene@gmail.com>
2018-05-02 22:06:51 -0700

    add rq worker process to docker-compose, fix redis port error

--- kryptos/app/utils/worker.py
+++ kryptos/app/utils/worker.py
@@ -3,9 +3,8 @@ import redis
 from rq import Worker, Queue, Connection
 
 listen = ["high", "default", "low"]
-redis_url = os.getenv("REDISTOGO_URL", "redis://localhost:6379")
 
-conn = redis.from_url(redis_url)
+conn = redis.Redis(host='redis', port=6379)
 
 if __name__ == "__main__":
     with Connection(conn):
