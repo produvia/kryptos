bd41ad673ca282cda4d6c66ee035a191e6f750c5
Cam Sweeney <cpsweene@gmail.com>
2018-05-03 03:04:02 -0700

    remove old export call

--- init_docker.sh
+++ init_docker.sh
@@ -2,7 +2,3 @@
 docker build -f Dockerfile-base -t kryptos-deps .
 docker build -t krytpos .
 docker-compose up -d
-
-export kryptos=sudo docker exec -i -t kryptos_web_1 /bin/bash
-
-# echo "\n\n$ Run 'sudo docker exec -i -t kryptos_web_1 /bin/bash' to enter the docker shell!"
