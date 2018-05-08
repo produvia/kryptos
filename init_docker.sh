c55d1b0bb147531765973a5ce7e30413af5b736c
Cam Sweeney <cpsweene@gmail.com>
2018-05-08 12:49:02 -0700

    add start_docker shell script and use it to enter container upon init

--- init_docker.sh
+++ init_docker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-docker build -f Dockerfile-base -t kryptos-deps .
+docker build -f Dockerfile-deps -t kryptos-deps .
 docker build -t krytpos .
-docker-compose up -d
 
-export kryptos=sudo docker exec -i -t kryptos_web_1 /bin/bash
+# docker build -f Dockerfile-rq -t kryptos-rq-dash .
 
-# echo "\n\n$ Run 'sudo docker exec -i -t kryptos_web_1 /bin/bash' to enter the docker shell!"
+chmod u+x start_docker.sh
+
+./start_docker.sh
+
+
+
---
+++ start_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker-compose up -d
+docker exec -it web bash
