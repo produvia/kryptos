0ec1dd2b4b4f82ed78eb8bce50e03897ac2ddd83
Cam Sweeney <cpsweene@gmail.com>
2018-07-08 00:31:59 -0700

    use multiprocessing to start main and all workers as jobs are queued

--- kryptos/worker/worker.py
+++ kryptos/worker/worker.py
@@ -1,6 +1,9 @@
 import json
 import redis
 from rq import Queue, Connection, Worker
+import click
+import multiprocessing
+import time
 
 from kryptos.strategy import Strategy
 from kryptos.app.extensions import jsonrpc
@@ -11,16 +14,6 @@ host = 'redis' if in_docker() else 'localhost'
 CONN = redis.Redis(host=host, port=6379)
 
 
-
-def start_worker():
-    # import before starting worker to loading during worker process
-    from kryptos.strategy import Strategy
-    from kryptos.app.extensions import jsonrpc
-    from kryptos.utils.outputs import in_docker
-
-    with Connection(CONN):
-        worker = Worker(map(Queue, QUEUE_NAMES))
-        worker.work()
 
 def get_queue(queue_name):
     if queue_name in ['paper', 'live']:
@@ -46,17 +39,15 @@ def queue_strat(strat_json, live=False, simulate_orders=True):
 
     if live and simulate_orders:
         q = get_queue('live')
-        start_worker()
 
     elif live:
         q = get_queue('paper')
-        start_worker()
 
     else:
         q = get_queue('backtest')
 
 
-    job = q.enqueue(run_strat, job_id=strat.name, kwargs={'strat_json': strat_json, 'live': live, 'simulate_orders': simulate_orders})
+    job = q.enqueue(run_strat, job_id=strat.name, kwargs={'strat_json': strat_json, 'live': live, 'simulate_orders': simulate_orders}, timeout=-1)
 
     return job.id, q.name
 
@@ -76,5 +67,33 @@ def get_strat_data(strat_name, queue_name):
 
     return job.result, job.meta
 
+def workers_required():
+    paper_q, live_q = get_queue('paper'), get_queue('live')
+    total_queued = len(paper_q) + len(live_q)
+    return total_queued
+
+
+
+@click.command()
+def manage_workers():
+    # import before starting worker to loading during worker process
+    # from kryptos.strategy import Strategy
+    # from kryptos.app.extensions import jsonrpc
+    # from kryptos.utils.outputs import in_docker
+
+    #start main worker
+    with Connection(CONN):
+        multiprocessing.Process(target=Worker(QUEUE_NAMES).work).start()
+
+    # create live queus when needed
+    while True:
+        queue_names = ['paper', 'live']
+        with Connection(CONN):
+            if workers_required() > 0:
+                for i in range(workers_required()):
+                    multiprocessing.Process(target=Worker(queue_names).work, kwargs={'burst': True}).start()
+            else:
+                time.sleep(5)
+
 if __name__ == '__main__':
-    start_worker()
+    manage_workers()
