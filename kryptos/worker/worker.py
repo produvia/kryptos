c24a1a533380c175fe3d38b6eac95929a91b558c
Cam Sweeney <cpsweene@gmail.com>
2018-07-09 12:01:02 -0700

    pass strat name to queued function so strat can access the correct job

--- kryptos/worker/worker.py
+++ kryptos/worker/worker.py
@@ -21,7 +21,7 @@ def get_queue(queue_name):
     return Queue(queue_name, connection=CONN)
 
 
-def run_strat(strat_json, live=False, simulate_orders=True):
+def run_strat(strat_json, strat_name, live=False, simulate_orders=True):
     strat_dict = json.loads(strat_json)
     strat = Strategy()
     strat.load_from_dict(strat_dict)
@@ -37,34 +37,27 @@ def queue_strat(strat_json, live=False, simulate_orders=True):
     strat.load_from_dict(strat_dict)
 
     if live and simulate_orders:
-        q = get_queue('live')
+        q = get_queue('paper')
 
     elif live:
-        q = get_queue('paper')
+        q = get_queue('live')
 
     else:
         q = get_queue('backtest')
 
-
-    job = q.enqueue(run_strat, job_id=strat.name, kwargs={'strat_json': strat_json, 'live': live, 'simulate_orders': simulate_orders}, timeout=-1)
+    job = q.enqueue(
+        run_strat,
+        job_id=strat.name,
+        kwargs={
+            'strat_json': strat_json,
+            'strat_name': strat.name, # pass to keep same id as the job_id
+            'live': live,
+            'simulate_orders': simulate_orders
+        },
+        timeout=-1)
 
     return job.id, q.name
 
-def get_strat_data(strat_name, queue_name):
-    q = get_queue(queue_name)
-
-    job = q.fetch_job(strat_name)
-    if job is None:
-        return
-
-    if job.is_failed:
-        info = job.exc_info.strip().split('\n')[-1]
-        raise Exception('Strat job failed: {}'.format(info))
-
-    if job.result is None:
-        return job.meta
-
-    return job.result, job.meta
 
 def workers_required():
     paper_q, live_q = get_queue('paper'), get_queue('live')
