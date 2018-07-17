83c0813a7eab9a210133dc7e4f559242c3b5e33d
Cam Sweeney <cpsweene@gmail.com>
2018-07-17 03:42:45 -0700

    add cors in extensions

--- kryptos/app/extensions.py
+++ kryptos/app/extensions.py
@@ -2,6 +2,7 @@
 """Extensions module. Each extension is initialized in the app factory located in app.py."""
 from flask_jsonrpc import JSONRPC
 from flask_bootstrap import Bootstrap
-
+from flask_cors import CORS
 jsonrpc = JSONRPC(service_url="/api", enable_web_browsable_api=True)
 bootstrap = Bootstrap()
+cors = CORS()
