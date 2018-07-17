4ab7f07ef5b991b0f7b160b4c17ac4e24777f400
Cam Sweeney <cpsweene@gmail.com>
2018-07-17 11:11:53 -0700

    remove containers before starting, build frontend for dev-build

--- docker_scripts/dev-build.sh
+++ docker_scripts/dev-build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+ids=$(docker ps -a -q)
+for id in $ids
+do
+  echo "$id"
+  docker stop $id && docker rm $id
+done
+
+echo "Rebuilding frontend"
+rm -rf kryptos/app/static/spa-mat
+cd frontend/ quasar clean && quasar build && cd ..
+cp -r frontend/dist/spa-mat kryptos/app/static/spa-mat
 
 docker build -t krpytos-dev -f Dockerfile .
--- docker_scripts/prod-build.sh
+++ docker_scripts/prod-build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+ids=$(docker ps -a -q)
+for id in $ids
+do
+  echo "$id"
+  docker stop $id && docker rm $id
+done
+
 docker build -t kryptos-prod:latest -f Dockerfile .
