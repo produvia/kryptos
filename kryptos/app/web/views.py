7baf300e29a9e926b19ec00cecdb8e25d664b7c4
Cam Sweeney <cpsweene@gmail.com>
2018-07-19 12:36:52 -0700

    set blueprint static folder correctly

--- kryptos/app/web/views.py
+++ kryptos/app/web/views.py
@@ -11,19 +11,12 @@ from kryptos.app.extensions import jsonrpc
 from kryptos.utils.outputs import in_docker
 from kryptos.worker import worker
 
-blueprint = Blueprint("public", __name__, url_prefix='/', static_folder='../static/spa-mat')
+blueprint = Blueprint("public", __name__, url_prefix='/', static_folder='static/spa-mat')
 
 
 @blueprint.route('/', defaults={'path': ''})
 @blueprint.route('/<path:path>')
 def route_frontend(path):
-    # ...could be a static file needed by the front end that
-    # doesn't use the `static` path (like in `<script src="bundle.js">`)
-    # return render_template('index.html')
-
-    if not in_docker():
-        import requests
-        return requests.get('http://localhost:8080/{}'.format(path)).text
     file_path = os.path.join(blueprint.static_folder, path)
     if os.path.isfile(file_path):
         return send_file(file_path)
