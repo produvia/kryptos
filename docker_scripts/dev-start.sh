5987a2d4f5936550a771700898a428c002d40faa
Cam Sweeney <cpsweene@gmail.com>
2018-07-24 03:26:44 -0700

    remove npm build command

--- docker_scripts/dev-start.sh
+++ docker_scripts/dev-start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# runs quasar dev server and starts docker container to allow live reloads
-cd frontend && node node_modules/quasar-cli/bin/quasar-dev &
 docker-compose -f docker-compose.dev.yaml up -d
-docker-compose exec worker /bin/bash
+
+docker-compose exec web flask db upgrade
+# docker logs -f web
+# docker exec -it db /bin/bash
