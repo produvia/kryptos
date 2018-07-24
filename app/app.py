04a2e68aa55efb74548065b58c1847356a8aa791
Cam Sweeney <cpsweene@gmail.com>
2018-07-23 21:26:18 -0700

    remove account blueprint from app setup

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_user import UserManager
 import rq_dashboard
 
-from app import web, api, bot, account, models
+from app import web, api, bot, models
 from app.extensions import jsonrpc, cors, db, migrate
 from app.settings import ProdConfig
 
@@ -54,7 +54,6 @@ def register_blueprints(app):
     """
     app.register_blueprint(web.views.blueprint)
     app.register_blueprint(api.views.api)
-    app.register_blueprint(account.views.blueprint)
     app.register_blueprint(bot.assistant.blueprint)
     app.register_blueprint(rq_dashboard.blueprint, url_prefix="/rq")
 
