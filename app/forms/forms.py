17bc2e45547f210b1169f0e2408dfee4e3042f69
Cam Sweeney <cpsweene@gmail.com>
2018-07-26 14:04:56 -0700

    use indicator abbreviation as the indicator select option data value

--- app/forms/forms.py
+++ app/forms/forms.py
@@ -11,13 +11,15 @@ datasets = [('None', 'None'), ('Google Trends, google'), ('Quandl Blochain Data'
 
 
 
-def indicator_group_name_selectors():
+def indicator_group_name_selectors() -> [(str, str)]:
+    """Returns list of select options of indicator group names"""
     selectors = []
     for k in ta.get_function_groups().keys():
         selectors.append((k, k))
     return selectors
 
-def all_indicator_selectors():
+def all_indicator_selectors() -> [(str, str)]:
+    """Returns the entire list of possible indicator abbreviation select options"""
     selectors = []
     for i in ta.get_functions():
         selectors.append((i, i))
@@ -29,14 +31,15 @@ def _get_indicator_params(indicator_abbrev):
     return func.parameters
 
 
-def get_indicators_by_group(group):
+def get_indicators_by_group(group: str) -> [(str, str)]:
+    """Returns list of select options containing abbreviations of the groups indicators"""
     indicator_selects = []
     group_indicators = ta.get_function_groups()[group]
     for i in range(len(group_indicators)):
         abbrev = group_indicators[i]
         func = getattr(ab, abbrev)
         name = func.info['display_name']
-        indicator_selects.append((name, abbrev))
+        indicator_selects.append((abbrev, abbrev))
 
     return indicator_selects
 
