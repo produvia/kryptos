c742d635894fb82e9ec79aae98b819a0792d1352
Cam Sweeney <cpsweene@gmail.com>
2018-08-01 12:14:52 -0700

    change redis instance, get password from datastore

--- app/app/settings.py
+++ app/app/settings.py
@@ -85,7 +85,7 @@ class ProdConfig(Config):
     TELEGRAM_TOKEN = get_from_datastore('TELEGRAM_TOKEN', 'production')
     REDIS_HOST = os.getenv('REDIS_HOST', '10.138.0.4')
     REDIS_PORT = os.getenv('REDIS_PORT', 6379)
-    REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', None)
+    REDIS_PASSWORD = get_from_datastore('REDIS_PASSWORD', 'production')
 
 
 
