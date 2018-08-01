5a9d9277c11a4e33ba12263c9581c44a4e7c7aa2
Cam Sweeney <cpsweene@gmail.com>
2018-07-31 19:08:47 -0700

    add missing import

--- app/app/web/public/views.py
+++ app/app/web/public/views.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 import os
 from flask import Blueprint, render_template
-from flask_user import current_user
+from flask_user import current_user, current_app
 import redis
-
+import logging
 # Grouping 2 blueprints together
 blueprint = Blueprint('public', __name__, url_prefix='/')
 
+
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+REDI_PORT = int(os.environ.get('REDIS_PORT', 6379))
+redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDI_PORT)
 
 @blueprint.route('/')
 def home_page():
     return render_template('public/landing.html', current_user=current_user)
 
 
-redis_host = os.environ.get('REDISHOST', 'localhost')
-redis_port = int(os.environ.get('REDISPORT', 6379))
-redis_client = redis.StrictRedis(host=redis_host, port=redis_port)
-
-
 @blueprint.route('/testredis')
 def index():
+    current_app.logger.warn(f'Testing Redis Conenction: {REDIS_HOST}:{REDI_PORT}')
     value = redis_client.incr('counter', 1)
     return 'Visitor number: {}'.format(value)
 
