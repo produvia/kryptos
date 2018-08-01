f75febacc4b5cf194d706a32bbf7cc02b64752af
Cam Sweeney <cpsweene@gmail.com>
2018-07-31 19:08:19 -0700

    don't use json file for bot_token

--- app/app/bot/bot_utils.py
+++ app/app/bot/bot_utils.py
@@ -5,9 +5,10 @@ from telegram import Bot
 from google.cloud import datastore
 
 
-bot = datastore.Client.from_service_account_json('kryptos-stage-a7f9fd94cd62.json')
+
 
 def send_to_user(text, user):
+    telegram_bot = datastore.Client(current_app.config['TELEGRAM_TOKEN'])
     if not user.telegram_id:
         current_app.logger.error(f'User {user} has no user_chat_id or telegram_id')
         raise ValueError('Can not send message to user with out chat_id')
