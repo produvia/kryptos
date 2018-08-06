c72414a46b81b0413146762910bc83590fcfc8e2
Cam Sweeney <cpsweene@gmail.com>
2018-08-06 14:46:48 -0700

    remove old files

--- docker_scripts/dev-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker-compose -f docker-compose.dev.yaml up -d
-
-docker-compose exec web flask db upgrade
-docker-compose logs -f web worker
-# docker exec -it db /bin/bash
--- docker_scripts/ingest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker exec -it worker sh -c "catalyst ingest-exchange -x bitfinex -f daily,minute &&
-    catalyst ingest-exchange -x poloniex -f daily,minute &&
-    catalyst ingest-exchange -x bitfinex -f daily,minute &&
-    /bin/bash"
