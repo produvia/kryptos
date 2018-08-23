57c24bfb81c227861570ec96096ed7621f10c2b7
Cam Sweeney <cpsweene@gmail.com>
2018-08-23 00:49:12 -0700

    add simple retry handler for workers

--- core/worker.py
+++ core/worker.py
@@ -1,7 +1,7 @@
 import os
 import json
 import redis
-from rq import Queue, Connection, Worker
+from rq import Queue, Connection, Worker, get_failed_queue
 import click
 import multiprocessing
 import time
@@ -15,6 +15,7 @@ from rq.contrib.sentry import register_sentry
 from kryptos import logger_group
 from kryptos.strategy import Strategy
 from kryptos.utils.outputs import in_docker
+from kryptos.utils import tasks
 from kryptos.settings import QUEUE_NAMES
 
 
@@ -26,6 +27,7 @@ REDIS_PORT = os.getenv('REDIS_PORT', 6379)
 REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', None)
 
 CONN = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+
 
 log = logbook.Logger('WorkerManager')
 logger_group.add_logger(log)
@@ -69,12 +71,12 @@ def manage_workers():
         multiprocessing.Process(target=backtest_worker.work).start()
 
         log.info('Starting worker for PAPER queues')
-        paper_worker = Worker(['paper'])
+        paper_worker = Worker(['paper'], exception_handlers=[retry_handler])
         register_sentry(client, paper_worker)
         multiprocessing.Process(target=paper_worker.work).start()
 
         log.info('Starting worker for LIVE queues')
-        live_worker = Worker(['live'])
+        live_worker = Worker(['live'], exception_handlers=[retry_handler])
         register_sentry(client, live_worker)
         multiprocessing.Process(target=live_worker.work).start()
 
@@ -85,13 +87,43 @@ def manage_workers():
                 log.debug(f"{required} workers required for {q}")
                 for i in range(required):
                     log.info(f"Creating {q} worker")
-                    worker = Worker([q])
+                    worker = Worker([q], exception_handlers=[retry_handler])
                     register_sentry(client, live_worker)
                     multiprocessing.Process(target=worker.work, kwargs={'burst': True}).start()
 
 
             time.sleep(5)
 
+
+
+
+def retry_handler(job, exc_type, exc_value, traceback):
+    MAX_FAILURES = 3
+    job.meta.setdefault('failures', 0)
+    job.meta['failures'] += 1
+
+    # Too many failures
+    if job.meta['failures'] >= MAX_FAILURES:
+        log.warn('job %s: failed too many times times - moving to failed queue' % job.id)
+        job.save()
+        tasks.queue_notification(f'Strategy has failed', job.meta['telegram_id'])
+        return True
+
+    # Requeue job and stop it from being moved into the failed queue
+    log.warn('job %s: failed %d times - retrying' % (job.id, job.meta['failures']))
+
+    fq = get_failed_queue()
+    fq.quarantine(job, Exception('Some fake error'))
+    # assert fq.count == 1
+
+    job.meta['failures'] += 1
+    job.save()
+    fq.requeue(job.id)
+
+    # Can't find queue, which should basically never happen as we only work jobs that match the given queue names and
+    # queues are transient in rq.
+    log.warn('job %s: cannot find queue %s - moving to failed queue' % (job.id, job.origin))
+    return True
 
 
 if __name__ == '__main__':
