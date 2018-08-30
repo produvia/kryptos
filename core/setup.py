26c0fe51ec2fd850f9dd31df3c5eb0a6e6da920f
Cam Sweeney <cpsweene@gmail.com>
2018-08-30 00:47:40 -0700

    remove old scripts

--- core/setup.py
+++ core/setup.py
@@ -43,11 +43,6 @@ setup(
     install_requires=requires,
     entry_points='''
         [console_scripts]
-        benchmark=kryptos.scripts.run_benchmark:benchmark
-        compare_all_strategies=kryptos.scripts.run_strategies:run
-        metrics=kryptos.scripts.run_metrics:run
-        compare=kryptos.scripts.compare:run
-        ta=kryptos.scripts.run_ta:run
         bchain=kryptos.scripts.bchain_activity:run
         trends=kryptos.scripts.trends:run
         strat=kryptos.scripts.build_strategy:run
