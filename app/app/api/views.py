fdb0e28629aaa3e0d57941479135441949316955
Cam Sweeney <cpsweene@gmail.com>
2018-08-30 23:36:08 -0700

    add simple endpoint to queue strats via api

--- app/app/api/views.py
+++ app/app/api/views.py
@@ -19,3 +19,22 @@ def strat_status():
     data = task.get_job_data(strat_id, queue_name)
 
     return jsonify(strat_info=data)
+
+@api.route('/strat', methods=['POST', "GET"])
+def run_strat():
+    data = request.json
+    current_app.logger.error(data)
+    strat_dict = data.get('strat_json')
+    queue_name = data.get('queue_name')
+
+    live, simulate_orders = False, True
+    if queue_name in ['paper', 'live']:
+        live = True
+
+    if queue_name == 'live':
+        simulate_orders = False
+
+    current_app.logger(f'Enqueuing strat to {queue_name} queue')
+    job_id, _ = task.queue_strat(strat_dict, user_id=None, live=False, simulate_orders=True)
+    current_app.logger.info(f"Strat running in job {job_id}")
+    return jsonify(strat_id=job_id)
