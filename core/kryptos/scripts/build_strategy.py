7e33e295ac7015144e6177aacce05f4bc1d33293
Cam Sweeney <cpsweene@gmail.com>
2018-08-31 13:48:33 -0700

    clean up strat command output

--- core/kryptos/scripts/build_strategy.py
+++ core/kryptos/scripts/build_strategy.py
@@ -14,7 +14,7 @@ from kryptos.utils.load import get_strat
 from kryptos.utils import tasks
 
 REMOTE_BASE_URL = 'https://kryptos-205115.appspot.com'
-LOCAL_BASE_URL = "https://web:8080"
+LOCAL_BASE_URL = "http://web:8080"
 
 
 @click.command()
@@ -42,36 +42,16 @@ LOCAL_BASE_URL = "https://web:8080"
 @click.option("--worker", "-w", is_flag=True, help="Run the strategy inside an RQ worker")
 @click.option("--hosted", "-h", is_flag=True, help="Run on a GCP instance via the API")
 def run(market_indicators, machine_learning_models, dataset, columns, data_indicators, json_file, python_script, paper, api, worker, hosted):
-
     if api and worker:
         if not hosted:
             click.secho('Providing the `--worker` flag is not required when using the api', fg='yellow')
         else:
             click.echo('')
 
-    strat = Strategy()
+    strat = load_from_cli(market_indicators, machine_learning_models, dataset,
+                          columns, data_indicators, json_file, python_script)
 
-    if python_script is not None:
-        strat = get_strat(python_script)
-
-    columns = list(columns)
-
-    for i in market_indicators:
-        strat.add_market_indicator(i.upper())
-
-    for i in machine_learning_models:
-        strat.add_ml_models(i.upper())
-
-    # currently assigns -i indicator to the column provided at the same index
-    if dataset is not None:
-        strat.use_dataset(dataset, columns)
-        for i, ind in enumerate(data_indicators):
-            strat.add_data_indicator(dataset, ind.upper(), col=columns[i])
-
-    if json_file is not None:
-        strat.load_json_file(json_file)
-
-    click.secho(strat.serialize(), fg="white")
+    click.secho(f'Running in Paper mode: {paper}', fg='yellow')
 
     if api or hosted:
         run_from_api(strat, paper, hosted)
@@ -80,10 +60,42 @@ def run(market_indicators, machine_learning_models, dataset, columns, data_indic
         run_in_worker(strat, paper)
 
     else:
+        click.secho('Running locally w/o worker (ML still requires a worker process)', fg='cyan')
         viz = not in_docker()
         strat.run(live=paper, viz=viz)
         result_json = strat.quant_results.to_json()
         display_summary(result_json)
+
+
+def load_from_cli(market_indicators, machine_learning_models, dataset, columns, data_indicators, json_file, python_script):
+    strat = Strategy()
+
+    if python_script is not None:
+        strat = get_strat(python_script)
+
+    columns = list(columns)
+
+    for i in market_indicators:
+        click.echo(f'Attaching indicator: {i.upper()}')
+        strat.add_market_indicator(i.upper())
+
+    for i in machine_learning_models:
+        click.echo(f'Attaching ml model: {i.upper()}')
+        strat.add_ml_models(i.upper())
+
+    # currently assigns -i indicator to the column provided at the same index
+    if dataset is not None:
+        click.echo(f'Using dataset: {dataset}')
+        strat.use_dataset(dataset, columns)
+        for i, ind in enumerate(data_indicators):
+            strat.add_data_indicator(dataset, ind.upper(), col=columns[i])
+
+    if json_file is not None:
+        click.echo(f'Loading from json file {json_file}')
+        strat.load_json_file(json_file)
+
+    click.secho(strat.serialize(), fg="white")
+    return strat
 
 
 def display_summary(result_json):
@@ -96,9 +108,12 @@ def display_summary(result_json):
 
 
 def run_from_api(strat, paper, hosted=False):
+    click.secho('Running strat via API', fg='cyan')
     if hosted:
+        click.secho('Running remotely', fg='yellow')
         base_url = REMOTE_BASE_URL
     else:
+        click.secho('Running locally', fg='yellow')
         base_url = LOCAL_BASE_URL
 
     api_url = os.path.join(base_url, 'api')
@@ -114,7 +129,7 @@ def run_from_api(strat, paper, hosted=False):
     }
 
     endpoint = os.path.join(api_url, 'strat')
-    click.echo(f'Enqueuing strategy at {endpoint}')
+    click.secho(f'Enqueuing strategy at {endpoint} on queue {q_name}', fg='yellow')
 
     resp = requests.post(endpoint, json=data)
     click.echo(resp)
@@ -142,6 +157,7 @@ def run_from_api(strat, paper, hosted=False):
 
 
 def run_in_worker(strat, paper):
+    click.secho('Running local strategy using worker', fg='cyan')
     if paper:
         q_name = 'paper'
     else:
