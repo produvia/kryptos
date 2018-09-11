369d94e0217162ab40ad0fb8eb532d4083096711
Bukosabino <bukosabino@gmail.com>
2018-09-11 16:06:23 +0200

    refactor code

--- ml/ml/utils/feature_exploration.py
+++ ml/ml/utils/feature_exploration.py
@@ -6,52 +6,41 @@ import shap
 
 from ml.utils import get_algo_dir
 
+
 def visualize_model(model, X, idx, configuration, namespace, name):
+
     if configuration['enabled'] and idx % configuration['iterations'] == 0:
 
         explainer = shap.TreeExplainer(model)
         shap_values = explainer.shap_values(X)
         shap.summary_plot(shap_values, X, plot_type="bar", show=False)
-
-        # save files
-        folder_path = get_algo_dir(namespace)
-        f_path = os.path.join(folder_path, "{}_{}_analyze_shap_features.png".format(name, idx))
-        plt.savefig(f_path, bbox_inches="tight", dpi=300)
+        save_fig(namespace, name, idx, importance_type='shap')
 
         if name == 'XGBOOST':
-            # TODO: exportar codigo a una función.
-            importance_type = 'weight'
-            xgb.plot_importance(model, importance_type=importance_type, max_num_features=20)
-            folder_path = get_algo_dir(namespace)
-            f_path = os.path.join(folder_path, "{}_{}_analyze_xgboost_{}_features.png".format(name, idx, importance_type))
-            plt.savefig(f_path, bbox_inches="tight", dpi=300)
-
-            importance_type = 'cover'
-            xgb.plot_importance(model, importance_type=importance_type, max_num_features=20)
-            folder_path = get_algo_dir(namespace)
-            f_path = os.path.join(folder_path, "{}_{}_analyze_xgboost_{}_features.png".format(name, idx, importance_type))
-            plt.savefig(f_path, bbox_inches="tight", dpi=300)
-
-            importance_type = 'gain'
-            xgb.plot_importance(model.get_score(fmap='', importance_type=importance_type), importance_type=importance_type, max_num_features=20)
-            folder_path = get_algo_dir(namespace)
-            f_path = os.path.join(folder_path, "{}_{}_analyze_xgboost_{}_features.png".format(name, idx, importance_type))
-            plt.savefig(f_path, dpi="figure")
+            for i in ['weight', 'cover', 'gain']:
+                if i == 'gain':
+                    xgb.plot_importance(model.get_score(fmap='', importance_type=i), importance_type=i, max_num_features=20)
+                else:
+                    xgb.plot_importance(model, importance_type=i, max_num_features=20)
+                save_fig(namespace, name, idx, importance_type=i)
 
         elif name == 'LIGHTGBM':
-
-            importance_type = 'split'
-            lgb.plot_importance(model, importance_type=importance_type, max_num_features=20)
-            folder_path = get_algo_dir(namespace)
-            f_path = os.path.join(folder_path, "{}_{}_analyze_lightgbm_{}_features.png".format(name, idx, importance_type))
-            plt.savefig(f_path, bbox_inches="tight", dpi=300)
-
-            importance_type = 'gain'
-            lgb.plot_importance(model, importance_type=importance_type, max_num_features=20)
-            folder_path = get_algo_dir(namespace)
-            f_path = os.path.join(folder_path, "{}_{}_analyze_lightgbm_{}_features.png".format(name, idx, importance_type))
-            plt.savefig(f_path, bbox_inches="tight", dpi=300)
+            for i in ['split', 'gain']:
+                lgb.plot_importance(model, importance_type=i, max_num_features=20)
+                save_fig(namespace, name, idx, importance_type=i)
 
         else:
-
             pass
+
+# TODO: fusion with Cam method
+def save_fig(namespace, name, idx, importance_type):
+
+    # TODO: meter carpeta intermedia "feature_exploration"
+
+    folder_path = get_algo_dir(namespace)
+    f_path = os.path.join(folder_path, "{}_{}_analyze_{}_features.png".format(name, idx, importance_type))
+
+    if importance_type == 'gain' and name == 'XGBOOST':
+        plt.savefig(f_path, dpi='figure')
+    else:
+        plt.savefig(f_path, bbox_inches="tight", dpi=300)
