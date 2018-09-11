0585d66c390c75afd5b22e075ec6c611c617696f
Bukosabino <bukosabino@gmail.com>
2018-09-11 15:17:58 +0200

    add lightgbm feature exploration

--- ml/ml/utils/feature_exploration.py
+++ ml/ml/utils/feature_exploration.py
@@ -1,6 +1,7 @@
 import os
 import matplotlib.pyplot as plt
 import xgboost as xgb
+import lightgbm as lgb
 import shap
 
 from ml.utils import get_algo_dir
@@ -19,7 +20,6 @@ def visualize_model(model, X, idx, configuration, namespace, name):
 
         if name == 'XGBOOST':
             # TODO: exportar codigo a una función.
-
             importance_type = 'weight'
             xgb.plot_importance(model, importance_type=importance_type, max_num_features=20)
             folder_path = get_algo_dir(namespace)
@@ -39,4 +39,19 @@ def visualize_model(model, X, idx, configuration, namespace, name):
             plt.savefig(f_path, dpi="figure")
 
         elif name == 'LIGHTGBM':
+
+            importance_type = 'split'
+            lgb.plot_importance(model, importance_type=importance_type, max_num_features=20)
+            folder_path = get_algo_dir(namespace)
+            f_path = os.path.join(folder_path, "{}_{}_analyze_lightgbm_{}_features.png".format(name, idx, importance_type))
+            plt.savefig(f_path, bbox_inches="tight", dpi=300)
+
+            importance_type = 'gain'
+            lgb.plot_importance(model, importance_type=importance_type, max_num_features=20)
+            folder_path = get_algo_dir(namespace)
+            f_path = os.path.join(folder_path, "{}_{}_analyze_lightgbm_{}_features.png".format(name, idx, importance_type))
+            plt.savefig(f_path, bbox_inches="tight", dpi=300)
+
+        else:
+
             pass
