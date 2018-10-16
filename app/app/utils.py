61e2db8d0549468fdcecf4573c4751133429f90c
treethought <cpsweene@gmail.com>
2018-10-15 17:06:34 -0700

    Add utils to encrypt auth before upload to storage

---
+++ app/app/utils.py
@@ -0,0 +1,40 @@
+import os
+import json
+from typing import Dict, TYPE_CHECKING, Tuple
+from flask import current_app
+from google.cloud import storage, kms_v1
+from google.cloud.kms_v1 import enums
+
+key_client = kms_v1.KeyManagementServiceClient()
+storage_client = storage.Client()
+def encrypt_user_auth(exchange_dict, user_id):
+    exchange_name = exchange_dict['name']
+    current_app.logger.info(f'Encrypting User {user_id} exchange auth for {exchange_name}')
+
+    key_path = key_client.crypto_key_path_path(
+        current_app.config['PROJECT_ID'],
+        'global',
+        'exchange_auth',
+        f"{exchange_name}_{user_id}_key"
+    )
+
+    plaintext = json.dumps(exchange_dict).encode()
+    resp = key_client.encrypt(key_path, plaintext)
+    current_app.logger.info('Successfully encrypted auth info')
+    return resp.ciphertext
+
+
+def upload_encrypted_auth(encrypted_text: bytes, user_id: int, exchange: str) -> Tuple[str, str]:
+    current_app.logger.info(f'Uploading encrypted {exchange} auth for user {user_id}')
+
+    blob_name = f"auth_{exchange}_{user_id}_json"
+    auth_bucket = storage_client.get_bucket('catalyst_auth')
+    blob = auth_bucket.blob(blob_name)
+    blob.upload_from_string(encrypted_text)
+    current_app.logger.info(f'Uploaded encrypted auth with blob name {blob_name}')
+    return blob_name, auth_bucket.name
+
+
+def upload_user_auth(exchange_dict, user_id):
+    encrypted = encrypt_user_auth(exchange_dict, user_id)
+    return upload_encrypted_auth(encrypted, user_id, exchange_dict['name'])
--- app/app/web/account/views.py
+++ app/app/web/account/views.py
@@ -1,53 +1,68 @@
 # -*- coding: utf-8 -*-
 import os
 import json
-from flask import send_file, Blueprint, redirect, current_app, render_template, request, url_for, flash, jsonify, session
+from flask import Blueprint, redirect, current_app, render_template, request, url_for, flash
 from flask_user import current_user, login_required
 from google.cloud import storage
 
 from app.extensions import db
 from app.forms import forms
-from app.models import User, StrategyModel
+from app.models import User
+from app.utils import upload_user_auth
 
 
+blueprint = Blueprint("account", __name__, url_prefix="/account")
 
-blueprint = Blueprint('account', __name__, url_prefix='/account')
 
 def telegram_auth_url():
-    return os.path.join(current_app.config['FRONTEND_URL'], '/account/telegram/authorize')
+    return os.path.join(current_app.config["FRONTEND_URL"], "/account/telegram/authorize")
 
-@blueprint.route('/')
+
+@blueprint.route("/")
 def user_account():
-    return render_template('account/dashboard.html', telegram_auth_url=telegram_auth_url(), telegram_bot=current_app.config['TELEGRAM_BOT'])
+    return render_template(
+        "account/dashboard.html",
+        telegram_auth_url=telegram_auth_url(),
+        telegram_bot=current_app.config["TELEGRAM_BOT"],
+    )
 
-@blueprint.route('/telegram')
+
+@blueprint.route("/telegram")
 def prompt_telegram():
-    return render_template('account/telegram_auth.html', telegram_auth_url=telegram_auth_url(), telegram_bot=current_app.config['TELEGRAM_BOT'])
+    return render_template(
+        "account/telegram_auth.html",
+        telegram_auth_url=telegram_auth_url(),
+        telegram_bot=current_app.config["TELEGRAM_BOT"],
+    )
 
-@blueprint.route('/telegram/logout')
+
+@blueprint.route("/telegram/logout")
 @login_required
 def telegram_logout():
     user = current_user
     user.unlink_telegram()
 
-    flash('Sucessfully logged out of Telegram!')
-    return render_template('account/dashboard.html')
+    flash("Sucessfully logged out of Telegram!")
+    return render_template("account/dashboard.html")
 
-@blueprint.route('/telegram/authorize')
+
+@blueprint.route("/telegram/authorize")
 @login_required
 def telegram_authorize():
-    telegram_id = request.args.get('id')
-    username = request.args.get('username')
-    photo_url = request.args.get('photo_url')
-    auth_date = request.args.get('auth_date')
-    has = request.args.get('hash)')
+    telegram_id = request.args.get("id")
+    username = request.args.get("username")
+    photo_url = request.args.get("photo_url")
+    auth_date = request.args.get("auth_date")
+    has = request.args.get("hash)")
 
     existing_linked_user = User.query.filter_by(telegram_id=telegram_id).first()
     if existing_linked_user is not None:
-        bot_name = current_app.config['TELEGRAM_BOT']
-        flash(f'Cannot link telegram account to more than one kryptos account.\n You can unlink your telegram account by sending "/logout" to @{bot_name} in telegram', category='error')
-        return redirect(url_for('account.user_account'))
-
+        bot_name = current_app.config["TELEGRAM_BOT"]
+        flash(
+            f'Cannot link telegram account to more than one kryptos account.\n You can unlink your telegram account by sending "/logout" to @{bot_name} in telegram',
+            category="error",
+        )
+        return redirect(url_for("account.user_account"))
 
     user = current_user
     user.telegram_id = telegram_id
@@ -57,47 +72,39 @@ def telegram_authorize():
 
     db.session.add(user)
     db.session.commit()
-    flash('Sucessfully logged in with Telegram!')
+    flash("Sucessfully logged in with Telegram!")
 
     message = "Thanks for signing up with Kryptos!\nType /menu to have a look around."
 
-    #TODO send message using updater
-    return render_template('account/dashboard.html')
+    # TODO send message using updater
+    return render_template("account/dashboard.html")
 
 
-@blueprint.route('/strategies')
+@blueprint.route("/strategies")
 @login_required
 def user_strategies():
-    return render_template('account/strategies.html')
+    return render_template("account/strategies.html")
 
 
-@blueprint.route('/exchanges', methods=['GET', 'POST'])
+@blueprint.route("/exchanges", methods=["GET", "POST"])
 @login_required
 def manage_exchanges():
     form = forms.UserExchangeKeysForm()
 
-    if request.method == 'POST' and form.validate():
+    if request.method == "POST" and form.validate():
 
         exchange_dict = {
-            'name': form.exchange.data,
-            'key': form.api_key.data,
-            'secret': form.api_secret.data
+            "name": form.exchange.data,
+            "key": form.api_key.data,
+            "secret": form.api_secret.data,
         }
 
-        blob_name = f"auth_{exchange_dict['name']}_{current_user.id}_json"
+        blob_name, auth_bucket = upload_user_auth(exchange_dict, current_user.id)
 
-        storage_client = storage.Client()
-        auth_bucket = storage_client.get_bucket('catalyst_auth')
-        blob = auth_bucket.blob(blob_name)
+        current_app.logger.warn(
+            "Exchange Auth {} uploaded to {} bucket.".format(blob_name, auth_bucket)
+        )
 
-        auth_json = json.dumps(exchange_dict)
+        return redirect(url_for("strategy.build_strategy"))
 
-        blob.upload_from_string(auth_json)
-
-        current_app.logger.warn('Exchange Auth {} uploaded to {} bucket.'.format(
-            blob_name,
-            auth_bucket))
-
-        return redirect(url_for('strategy.build_strategy'))
-
-    return render_template('account/user_exchanges.html', form=form)
+    return render_template("account/user_exchanges.html", form=form)
