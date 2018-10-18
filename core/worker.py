48897351093148d722ecf0e7aebdc9b56a8802e7
treethought <cpsweene@gmail.com>
2018-10-18 16:23:33 -0700

    don’t requeue job if failed from auth error

--- core/worker.py
+++ core/worker.py
@@ -21,22 +21,24 @@ from kryptos.utils import tasks
 from kryptos.settings import QUEUE_NAMES, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, SENTRY_DSN
 
 
-client = Client(SENTRY_DSN, transport=HTTPTransport)
+# client = Client(SENTRY_DSN, transport=HTTPTransport)
 
 CONN = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
 
 
-log = logbook.Logger('WorkerManager')
+log = logbook.Logger("WorkerManager")
 logger_group.add_logger(log)
-log.warn(f'Using Redis connection {REDIS_HOST}:{REDIS_PORT}')
+log.warn(f"Using Redis connection {REDIS_HOST}:{REDIS_PORT}")
 
 
 def get_queue(queue_name):
     return Queue(queue_name, connection=CONN)
 
 
-def run_strat(strat_json, strat_id, user_id=None, telegram_id=None, live=False, simulate_orders=True):
-    log.info(f'Worker received job for strat {strat_id}')
+def run_strat(
+    strat_json, strat_id, user_id=None, telegram_id=None, live=False, simulate_orders=True
+):
+    log.info(f"Worker received job for strat {strat_id}")
     strat_dict = json.loads(strat_json)
     strat = Strategy.from_dict(strat_dict)
     strat.id = strat_id
@@ -45,7 +47,7 @@ def run_strat(strat_json, strat_id, user_id=None, telegram_id=None, live=False, 
     strat.run(viz=False, live=live, simulate_orders=simulate_orders, user_id=user_id, as_job=True)
     result_df = strat.quant_results
     if result_df is None:
-        log.warning('No results from strategy')
+        log.warning("No results from strategy")
         return
 
     return result_df.to_json()
@@ -57,7 +59,7 @@ def workers_required(queue_name):
 
 
 def remove_zombie_workers():
-    log.warn('Removing zombie workers')
+    log.warn("Removing zombie workers")
     workers = Worker.all(connection=CONN)
     for worker in workers:
         if len(worker.queues) < 1:
@@ -66,19 +68,22 @@ def remove_zombie_workers():
             job = worker.get_current_job()
             if job is not None:
                 job.ended_at = datetime.datetime.utcnow()
-                worker.failed_queue.quarantine(job, exc_info=("Dead worker", "Moving job to failed queue"))
+                worker.failed_queue.quarantine(
+                    job, exc_info=("Dead worker", "Moving job to failed queue")
+                )
             worker.register_death()
+
 
 # TODO remove old workers that weren't removed during SIGKILL
 # these workers stay in redis memory and have a queue (not zombie) but no job
 # but they have actually been killed, and won't restart
 def remove_stale_workers():
-    log.warn('Removing stale workers')
+    log.warn("Removing stale workers")
     workers = Worker.all(connection=CONN)
     for worker in workers:
-        for q in ['paper', 'live', 'backtest']:
+        for q in ["paper", "live", "backtest"]:
             if q in worker.queue_names() and worker.get_current_job() is None:
-                log.warn('Removing stale worker {}'.format(worker))
+                log.warn("Removing stale worker {}".format(worker))
                 worker.clean_registries()
                 worker.register_death()
 
@@ -94,21 +99,21 @@ def manage_workers():
     # remove_stale_workers()
     # start main worker
     with Connection(CONN):
-        log.info('Starting initial workers')
+        log.info("Starting initial workers")
 
-        log.info('Starting worker for BACKTEST queue')
-        backtest_worker = Worker(['backtest'])
-        register_sentry(client, backtest_worker)
+        log.info("Starting worker for BACKTEST queue")
+        backtest_worker = Worker(["backtest"])
+        # register_sentry(client, backtest_worker)
         multiprocessing.Process(target=backtest_worker.work).start()
 
-        log.info('Starting worker for PAPER queues')
-        paper_worker = Worker(['paper'], exception_handlers=[retry_handler])
-        register_sentry(client, paper_worker)
+        log.info("Starting worker for PAPER queues")
+        paper_worker = Worker(["paper"], exception_handlers=[retry_handler])
+        # register_sentry(client, paper_worker)
         multiprocessing.Process(target=paper_worker.work).start()
 
-        log.info('Starting worker for LIVE queues')
-        live_worker = Worker(['live'], exception_handlers=[retry_handler])
-        register_sentry(client, live_worker)
+        log.info("Starting worker for LIVE queues")
+        live_worker = Worker(["live"], exception_handlers=[retry_handler])
+        # register_sentry(client, live_worker)
         multiprocessing.Process(target=live_worker.work).start()
 
         # create paper/live queues when needed
@@ -119,15 +124,27 @@ def manage_workers():
                     log.info(f"Creating {q} worker")
                     worker = Worker([q], exception_handlers=[retry_handler])
                     register_sentry(client, live_worker)
-                    multiprocessing.Process(target=worker.work, kwargs={'burst': True}).start()
+                    multiprocessing.Process(target=worker.work, kwargs={"burst": True}).start()
 
             time.sleep(5)
 
 
 def retry_handler(job, exc_type, exc_value, traceback):
     MAX_FAILURES = 3
-    job.meta.setdefault('failures', 0)
-    job.meta['failures'] += 1
+    job.meta.setdefault("failures", 0)
+    job.meta["failures"] += 1
+
+    from ccxt.base.errors import AuthenticationError
+    from catalyst.exchange.exchange_errors import ExchangeRequestError
+
+    auth_errors = [AuthenticationError, ExchangeRequestError]
+
+    if exc_type in auth_errors:
+        log.critical(exc_value)
+        job.save()
+        tasks.queue_notification(f"Auth error: {exc_value}", job.meta["telegram_id"])
+        return True
+
     #
     # if exc_type == ShutDownImminentException:
     #     fq = get_failed_queue()
@@ -142,28 +159,28 @@ def retry_handler(job, exc_type, exc_value, traceback):
     # return True
 
     # Too many failures
-    if job.meta['failures'] >= MAX_FAILURES:
-        log.warn('job %s: failed too many times times - moving to failed queue' % job.id)
+    if job.meta["failures"] >= MAX_FAILURES:
+        log.warn("job %s: failed too many times times - moving to failed queue" % job.id)
         job.save()
-        tasks.queue_notification(f'Strategy has failed', job.meta['telegram_id'])
+        tasks.queue_notification(f"Strategy has failed", job.meta["telegram_id"])
         return True
 
     # Requeue job and stop it from being moved into the failed queue
-    log.warn('job %s: failed %d times - retrying' % (job.id, job.meta['failures']))
+    log.warn("job %s: failed %d times - retrying" % (job.id, job.meta["failures"]))
 
     fq = get_failed_queue()
     fq.quarantine(job, exc_type(exc_value))
     # assert fq.count == 1
 
-    job.meta['failures'] += 1
+    job.meta["failures"] += 1
     job.save()
     fq.requeue(job.id)
 
     # Can't find queue, which should basically never happen as we only work jobs that match the given queue names and
     # queues are transient in rq.
-    log.warn('job %s: cannot find queue %s - moving to failed queue' % (job.id, job.origin))
+    log.warn("job %s: cannot find queue %s - moving to failed queue" % (job.id, job.origin))
     return True
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     manage_workers()
