86c44dfb60ade13689bf850536512dbaaa217521
treethought <cpsweene@gmail.com>
2018-10-25 13:19:05 -0700

    always kill via api

--- core/kryptos/scripts/kill_strat.py
+++ core/kryptos/scripts/kill_strat.py
@@ -1,13 +1,6 @@
 import os
-import json
-import time
 import click
 import requests
-
-from rq import Connection
-from kryptos.utils.outputs import in_docker
-from kryptos.worker.worker import StratWorker
-from kryptos.utils import tasks
 
 
 REMOTE_BASE_URL = "https://kryptos-205115.appspot.com"
@@ -16,37 +9,14 @@ LOCAL_BASE_URL = "http://web:8080"
 
 @click.command()
 @click.argument("strat_id", type=str)
-@click.option("--api", "-a", is_flag=True, help="Run the strategy via API")
 @click.option("--hosted", "-h", is_flag=True, help="Kill on a GCP instance via the API")
-def run(strat_id, api, hosted):
+def run(strat_id, hosted):
     click.secho(f"Killing strat {strat_id}", fg="yellow")
 
-    marked = False
-    job = None
-
-    if api or hosted:
-        kill_from_api(strat_id, hosted=hosted)
+    if hosted:
+        resp = kill_from_api(strat_id, hosted=hosted)
+        resp.raise_for_status()
         return
-
-    with Connection(tasks.CONN):
-        for w in StratWorker.all():
-            job = w.get_current_job()
-            if job and job.id == strat_id:
-                job.kill()
-                marked = True
-                break
-
-        if marked:
-            while job.get_status() == "started":
-                click.secho("Waiting for algo exit...")
-                click.secho(f"Status: {job.get_status()}")
-                time.sleep(2)
-
-            else:
-                click.secho("Successfuly shut down job")
-
-        else:
-            click.secho("Did not find job")
 
 
 def kill_from_api(strat_id, hosted=False):
@@ -67,4 +37,4 @@ def kill_from_api(strat_id, hosted=False):
 
     resp = requests.post(endpoint, json=data)
     click.echo(resp)
-    resp.raise_for_status()
+    return resp
